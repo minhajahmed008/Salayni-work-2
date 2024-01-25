982b1a3f65c316f11c4f7a36069a4e302f329c90
minhajahmed008 <155399680+minhajahmed008@users.noreply.github.com>
2024-01-25 05:35:27 +0500

    Update Question5.dart

--- Question5.dart
+++ Question5.dart
@@ -1,5 +1,16 @@
 void main() {
+print("Q.5: Create a map with name, phone keys and store some values to it.Usewhere to find all keys that have lenght 4.");
+Map  idealMap = {
+"Minhaj" : "748758",
+"Bilal" : "647857",
+"yasir" : "687808",
+"Fahad" : "879840",
+};
+List keysOfLength4 = idealMap.keys.where((key) => key.length == 4).toList();
 
+print("keysOfLength4 = idealMap : $keysOfLength4 = $idealMap");
+
+  
 
 
   

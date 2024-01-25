8822c6449199fc916107ce098453358e5e5e88a7
minhajahmed008 <155399680+minhajahmed008@users.noreply.github.com>
2024-01-26 00:05:15 +0500

    Create Question 6

---
+++ Question 6.dart
@@ -0,0 +1,22 @@
+void main() {
+print("Create Map variable name world then inside it create countries Map, Keywill be the name country & country value will have another map having capital City, currency and language to it.by using any country key print all the value of Capital & Currency.");
+var World = Map();
+World["pakistan"] = {
+"Capital City" : "Islmabad",
+"Currency" : "Pakistani Rupees PKR",
+"Language" : " Urdu"
+};
+
+World["Sudia Arabia"] = {
+"Capital City" : "Radiyah",
+"Currency" : " Riyal",
+"Language" : "Arabi"
+};
+
+World["USA"] = {
+"Capital City" : "Washinton dC",
+"Currency" : "Dollars",
+"Language" : "English"
+};
+World.forEach((k,v)=> print("${k}:${v}"));
+}
